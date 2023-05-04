f18fa2bc976e04fe5aae91cbf8c38b9e4463469b
imoliviant <112634843+imoliviant@users.noreply.github.com>
2023-05-04 20:30:10 +0900

    commit

--- additional/js/app.js
+++ additional/js/app.js
@@ -3,3 +3,16 @@ var contract = null;
 const oldTdhAddy = "0x1c2bB5d2812D307C2056C6A406d334676D067EE9";
 const newTdhAddy = "";
 const tdhMigratorAddy = "";
+
+document.getElementById('connectwallet').onclick = async () => {
+  if(window.ethereum){
+    await window.ethereum.send('eth_requestAccounts');
+    window.web3 = new Web3(window.ethereum);
+    var accounts = await web3.eth.getAccounts();
+    tdhUsers = accounts[0];
+    document.getElementById('connectwallet').textContent = "Connected!";
+    console.log(tdhUsers);
+    oldTdh = new web3.eth.Contract(tokenAbi, oldTdhAddy);
+  } else {
+  }
+}
