7e376a578286b041b933c2875de8ddfffe62079d
imoliviant <112634843+imoliviant@users.noreply.github.com>
2023-05-04 20:33:20 +0900

    commit

--- additional/js/app.js
+++ additional/js/app.js
@@ -1,8 +1,6 @@
 var tdhUsers = null;
 var contract = null;
 const oldTdhAddy = "0x1c2bB5d2812D307C2056C6A406d334676D067EE9";
-const newTdhAddy = "";
-const tdhMigratorAddy = "";
 
 document.getElementById('connectwallet').onclick = async () => {
   if(window.ethereum){
