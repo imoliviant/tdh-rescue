fe77d6b359ab1de4f5db67a0a841a79ae30d38d8
imoliviant <112634843+imoliviant@users.noreply.github.com>
2023-05-04 20:23:14 +0900

    commit

--- additional/js/app.js
+++ additional/js/app.js
@@ -1,5 +1,5 @@
 var tdhUsers = null;
 var contract = null;
-const oldTdhAddy = "";
+const oldTdhAddy = "0x1c2bB5d2812D307C2056C6A406d334676D067EE9";
 const newTdhAddy = "";
 const tdhMigratorAddy = "";
